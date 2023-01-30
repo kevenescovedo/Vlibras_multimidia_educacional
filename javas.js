2f2698b294adfdc5330a0f0e1b02f311a2f1649e
Edson Bila dos Santos Junior <88840886+EdsonBila@users.noreply.github.com>
2023-01-30 00:17:14 -0300

    Update javas.js

--- javas.js
+++ javas.js
@@ -1,73 +1,49 @@
-
-
-
 function preview() {
-
-    let ImportScriptFile =  () => {
-        let value =   window.localStorage.setItem('onvlibras',1);
-         window.localStorage.setItem('onclick',1);
-       
-        console.log(value);
-    
+  let ImportScriptFile = () => {
+    let value = window.localStorage.setItem('onvlibras', 1);
+    window.localStorage.setItem('onclick', 1);
     window.location.reload(true);
-    
-      }
-      ImportScriptFile();
+    setTimeout(() => {
+      document.querySelector(".access-button").click()
+    }, 100)
+  }
+  ImportScriptFile();
+}
+function script1() {
+  let value = window.localStorage.getItem('onvlibras');
+  if (value) {
+    let script = document.createElement('script');
+    script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
+    document.head.appendChild(script);
+    script.onload = () => {
+      script2(true);
     }
-      function script1() {
-     
-        let value =   window.localStorage.getItem('onvlibras');
-        console.log('valueeeeeeeeeee');
-        console.log(value);
-        if(value) {
-        
-          let script = document.createElement('script');
-          script.src = 'https://vlibras.gov.br/app/vlibras-plugin.js';
+  }
 
-          document.head.appendChild(script);
-          script.onload = () => {
-            script2(true);
-          }
-       
-         
-         
-        
-      }
-     
-     }
-      async function script2(keyRun) {
-      if(keyRun) {
-      new window.VLibras.Widget('https://vlibras.gov.br/app');
-     document.querySelector(".teste").style.display = "none";
-     if(  window.localStorage.getItem('onclick')) {
-
-     
-    
-     var mutationObserver = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-      
-       if(mutation.target.className == 'access-button' &&  window.localStorage.getItem('onclick') != 0) {
-        document.querySelector(".access-button").click();
-        window.localStorage.setItem('onclick',0);
-        mutationObserver = null;
-       };
-        
+}
+async function script2(keyRun) {
+  if (keyRun) {
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+    document.querySelector(".teste").style.display = "none";
+    if (window.localStorage.getItem('onclick')) {
+      var mutationObserver = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+          if (mutation.target.className == 'access-button' && window.localStorage.getItem('onclick') != 0) {
+            document.querySelector(".access-button").click();
+            window.localStorage.setItem('onclick', 0);
+          };
+        });
       });
-    });
-  mutationObserver.observe(document.documentElement, {
-  attributes: true,
-  characterData: true,
-  childList: true,
-  subtree: true,
-  attributeOldValue: true,
-  characterDataOldValue: true
-});
-
-     
-      }
+      mutationObserver.observe(document.documentElement, {
+        attributes: true,
+        characterData: true,
+        childList: true,
+        subtree: true,
+        attributeOldValue: true,
+        characterDataOldValue: true
+      });
     }
-       }
-      
-       script1();
-       
+  }
+}
 
+script1();
